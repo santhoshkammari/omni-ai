d8f7de920b27ddf71723ce9d85ffc8b547d01095
santhoshkammari <santhoshkammari1999@gmail.com>
2024-10-03 11:36:30 +0530

    streamlit version1 done

--- src/main/backend/streamlit_app.py
+++ src/main/backend/streamlit_app.py
@@ -7,13 +7,14 @@ st.set_page_config(layout="wide")
 
 class OmniAIChatApp:
     AVAILABLE_MODELS: List[str] = [
-        'meta-llama/Meta-Llama-3.1-70B-Instruct',
-        'CohereForAI/c4ai-command-r-plus-08-2024',
+        'microsoft/Phi-3.5-mini-instruct',
         'Qwen/Qwen2.5-72B-Instruct',
-        'meta-llama/Llama-3.2-11B-Vision-Instruct',
+        'meta-llama/Meta-Llama-3.1-70B-Instruct',
         'NousResearch/Hermes-3-Llama-3.1-8B',
         'mistralai/Mistral-Nemo-Instruct-2407',
-        'microsoft/Phi-3.5-mini-instruct'
+        'meta-llama/Llama-3.2-11B-Vision-Instruct',
+        'CohereForAI/c4ai-command-r-plus-08-2024',
+
     ]
 
     def __init__(self):

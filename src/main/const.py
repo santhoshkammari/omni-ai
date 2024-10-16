a804e60d2eea86bddd11f57c8c32e4bab65d40a2
santhoshkammari <santhoshkammari1999@gmail.com>
2024-10-17 01:54:07 +0530

    added nemotron

--- src/main/const.py
+++ src/main/const.py
@@ -2,15 +2,11 @@ from typing import List
 
 WORD_LLAMA_DIM:int = 1024  #Options: [64, 128, 256, 512, 1024]
 
-AVAILABLE_MODELS:List[str] = [
-    'Qwen/Qwen2.5-72B-Instruct',
-    'CohereForAI/c4ai-command-r-plus-08-2024',
-    'microsoft/Phi-3.5-mini-instruct',
-    'meta-llama/Meta-Llama-3.1-70B-Instruct',
-        'NousResearch/Hermes-3-Llama-3.1-8B',
-        'mistralai/Mistral-Nemo-Instruct-2407',
-        'meta-llama/Llama-3.2-11B-Vision-Instruct'
-    ]
+AVAILABLE_MODELS:List[str] = ['nvidia/Llama-3.1-Nemotron-70B-Instruct-HF','meta-llama/Meta-Llama-3.1-70B-Instruct', 'CohereForAI/c4ai-command-r-plus-08-2024',
+                     'Qwen/Qwen2.5-72B-Instruct',
+                     'meta-llama/Llama-3.2-11B-Vision-Instruct', 'NousResearch/Hermes-3-Llama-3.1-8B',
+                     'mistralai/Mistral-Nemo-Instruct-2407', 'microsoft/Phi-3.5-mini-instruct']
+
 AGENT_TYPES:List[str] = [
     "QuestionAnswer",
     "Reasoning",

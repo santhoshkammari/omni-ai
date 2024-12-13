09e005a9a73da1913bf65e357fa79d5845d6c499
santhoshkammari <santhoshkammari1999@gmail.com>
2024-12-14 03:58:47 +0530

    add google search functionality

--- aichatlite/core/features/feature_main.py
+++ aichatlite/core/features/feature_main.py
@@ -16,7 +16,10 @@ class FeatureHandlerMain:
             return self.chatbot.generator(self.query,
                                          system_prompt=self.system_prompt)
         elif self.agent_type in ["GoogleSearchAI","DeepGoogleSearchAI"]:
-            google_results = google_searchai(self.query) if self.agent_type=="GoogleSearchAI" else deep_google_search(self.query)
+            try:
+                google_results = google_searchai(self.query) if self.agent_type=="GoogleSearchAI" else deep_google_search(self.query)
+            except:
+                google_results = "No Results Found in Google Search"
             enhanced_query = (f"Google Results for {self.query} is {google_results}"
                               f"\n\n Now based on google results answer {self.query}"
                               f"Start by saying Using Google Results or Google results not Found"
